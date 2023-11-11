42445641a96c001881fb22d2944f0d3a8cbfc7f1
erikgab01 <erikgab01@gmail.com>
2023-11-12 01:25:10 +0500

    Readme update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-
-dotenv.config(); // load env vars from .env
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +7,7 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": {
-                target: process.env.BACKEND_URL,
+                target: "http://127.0.0.1:8000",
             },
         },
     },
